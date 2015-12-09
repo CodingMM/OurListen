6c61d942bc5b159e5499ade892347ab6d13a9367
eleanwei <eleanwei520@sina.com>
2015-12-09 17:25:34 +0800

    add api

---
+++ ListenProject/Base/API/DataAPI.h
@@ -0,0 +1,13 @@
+//
+//  DataAPI.h
+//  ListenProject
+//
+//  Created by Elean on 15/12/9.
+//  Copyright © 2015年 夏婷. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataAPI : NSObject
+
+@end
---
+++ ListenProject/Base/API/DataAPI.m
@@ -0,0 +1,13 @@
+//
+//  DataAPI.m
+//  ListenProject
+//
+//  Created by Elean on 15/12/9.
+//  Copyright © 2015年 夏婷. All rights reserved.
+//
+
+#import "DataAPI.h"
+
+@implementation DataAPI
+
+@end
