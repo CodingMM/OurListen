75f91e3ceef60f5931d3885ff0ba6b0eb980ca57
xiating <xialichen0127@sina.cn>
2015-12-09 21:21:06 +0800

    增加模块

---
+++ ListenProject/Controllers/Common/CCNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CCNavigationController.h
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/9.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCNavigationController : UINavigationController
+
+@end
---
+++ ListenProject/Controllers/Common/CCNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  CCNavigationController.m
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/9.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import "CCNavigationController.h"
+
+@interface CCNavigationController ()
+
+@end
+
+@implementation CCNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.automaticallyAdjustsScrollViewInsets
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
