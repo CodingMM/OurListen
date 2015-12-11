c57b4abfa35e492f3afb6cc70e50d54727517e11
xiating <xialichen0127@sina.cn>
2015-12-11 16:07:40 +0800

    修改目录结构

---
+++ ListenProject/Controllers/Common/CCBaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCBaseTableViewController.h
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/11.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCBaseTableViewController : UITableViewController
+
+@end
---
+++ ListenProject/Controllers/Common/Discover/home/CCDiscoverViewController.h
@@ -0,0 +1,17 @@
+//
+//  CCDiscoverViewController.h
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/9.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import "CCBaseViewController.h"
+
+@interface CCDiscoverViewController : CCBaseViewController
+
+
+
+
+
+@end
