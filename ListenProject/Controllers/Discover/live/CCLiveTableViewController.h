8f841b304b79954f562ad49528654f63edcb1599
xiating <xialichen0127@sina.cn>
2015-12-14 16:10:41 +0800

    添加CCRecomViewController、CCLiveTableViewController

---
+++ ListenProject/Controllers/Discover/live/CCLiveTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  CCLiveTableViewController.h
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/14.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCBaseTableViewController.h"
+
+@interface CCLiveTableViewController : CCBaseTableViewController
+
+@end
