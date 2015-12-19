11a49caab3698da0c9116d4ead24a8441fe2c525
xiating <xialichen0127@sina.cn>
2015-12-19 20:48:12 +0800

    直播cell

---
+++ ListenProject/View/Live/CCLiveMoreCollectionCell.m
@@ -0,0 +1,18 @@
+//
+//  CCLiveMoreCollectionCell.m
+//  ListenProject
+//
+//  Created by xiating on 15/12/19.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+
+#import "CCLiveMoreCollectionCell.h"
+
+@implementation CCLiveMoreCollectionCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
