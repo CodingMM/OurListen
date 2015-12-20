3c8718bdb27543fdf37a90316f9fa27bbf210d40
eleanwei <eleanwei520@sina.com>
2015-12-20 22:43:56 +0800

    add search

---
+++ ListenProject/Controllers/Discover/live/search/CCSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCSearchViewController.h
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/14.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import "CCBaseViewController.h"
+
+@interface CCSearchViewController : CCBaseViewController
+
+@end
---
+++ ListenProject/Controllers/Discover/live/search/CCSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  CCSearchViewController.m
+//  ListenProject
+//
+//  Created by 夏婷 on 15/12/14.
+//  Copyright (c) 2015年 夏婷. All rights reserved.
+//
+
+#import "CCSearchViewController.h"
+
+@interface CCSearchViewController ()
+
+@end
+
+@implementation CCSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
