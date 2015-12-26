ebc110f154e0a192b502f13d0245b8d5b7fc6df0
eleanwei <eleanwei520@sina.com>
2015-12-26 19:38:00 +0800

    增加更多模块

---
+++ ListenProject/Controllers/Discover/live/addMore/CCAddMoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCAddMoreViewController.h
+//  ListenProject
+//
+//  Created by Elean on 15/12/26.
+//  Copyright © 2015年 夏婷. All rights reserved.
+//
+
+#import "CCBaseViewController.h"
+
+@interface CCAddMoreViewController : CCBaseViewController
+
+@end
---
+++ ListenProject/Controllers/Discover/live/addMore/CCAddMoreViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCAddMoreViewController.m
+//  ListenProject
+//
+//  Created by Elean on 15/12/26.
+//  Copyright © 2015年 夏婷. All rights reserved.
+//
+
+#import "CCAddMoreViewController.h"
+
+@interface CCAddMoreViewController ()
+@property (nonatomic, strong)UITableView *tableView;
+@property (nonatomic, strong)NSMutableArray *dataArray;
+@end
+
+@implementation CCAddMoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
